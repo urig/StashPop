c550246129957ebf15d6d8cab72467e26c7ff377
Uri Goldstein <uri.goldstein@gmail.com>
2015-11-21 00:10:11 +0200

    Loading content.js as a content script for 'github.com' into the Firefox add-on.

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 var self = require('sdk/self');
+var pageMod = require("sdk/page-mod");
+
+// Nasty hack based on source code from sdk/self.js.
+// Lets us load 'content.js' from root, rather than from under /data
+// (which is what self.data.url() does).
+const options = require('@loader/options');
+const { get } = require("sdk/preferences/service");
+const { readURISync } = require('sdk/net/url');
+const id = options.id;
+const readPref = key => get("extensions." + id + ".sdk." + key);
+const name = readPref("name") || options.name;
+const baseURI = readPref("baseURI") || options.prefixURI + name + "/"
+const uri = (path="") =>
+  path.includes(":") ? path : baseURI + path.replace(/^\.\//, "");
+var selfDataHack = Object.freeze({
+  url: uri,
+  load: function read(path) {
+    return readURISync(uri(path));
+  }
+});
+// End of nasty hack
+
+pageMod.PageMod({
+  include: "*.github.com",
+  // Instead of self.data.url() we use selfDataHack.url()
+  contentScriptFile: selfDataHack.url("content.js")
+});
 
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
