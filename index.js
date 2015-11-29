6c3698e01af0ab40d022bfea5c78d3c86aa1c5a3
Uri Goldstein <uri.goldstein@gmail.com>
2015-11-29 20:56:36 +0200

    Now loading jquery and injectcode.js using page-mod.

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var self = require('sdk/self');
 var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
-	include: "*.github.com",
-	contentScriptFile:  "resource://stashpop/content.js"
+    include: "*.github.com",
+    contentScriptFile: ["resource://stashpop/jquery-2.1.4.min.js", "resource://stashpop/content.js"],
+    contentScriptWhen: "start"
+});
+
+pageMod.PageMod({
+    include: "*.github.com",
+    contentScriptFile: "resource://stashpop/scripts/injectedcode.js",
+    contentScriptWhen: "end"
 });
 
 // a dummy function, to show how tests work.
